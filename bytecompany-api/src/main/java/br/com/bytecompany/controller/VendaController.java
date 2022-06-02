84a381273c87a9edf1b4113cbf685b5461e80dfa
aljsjunca <aljs.junca@gmail.com>
2022-06-02 14:27:17 -0300

    Adicao do Método DELETE no CRUD das Vendas

--- bytecompany-api/src/main/java/br/com/bytecompany/controller/VendaController.java
+++ bytecompany-api/src/main/java/br/com/bytecompany/controller/VendaController.java
@@ -1,10 +1,11 @@
 package br.com.bytecompany.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,26 +18,30 @@ import br.com.bytecompany.model.Venda;
 import br.com.bytecompany.repository.VendaRepository;
 
 @RestController
-@RequestMapping("/vendas")
 public class VendaController {
 
 	@Autowired
 	private VendaRepository vendaRepository;
 	
-	@GetMapping
+	@GetMapping("/vendas")
 	public List<Venda> listar(){
 		return vendaRepository.findAll();
 	}
 	
 	@GetMapping("/vendas/{id}")
-	public Venda getVendaEspecifica(@PathVariable Long id) {
-		return vendaRepository.getById(id);
+	public Optional<Venda> getVendaEspecifica(@PathVariable Long id) {
+		return vendaRepository.findById(id);
 	}
 	
 	
-	@PostMapping
+	@PostMapping("/vendas")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Venda adicionar(@RequestBody Venda venda){
 		return vendaRepository.save(venda);
+	}
+	
+	@DeleteMapping("/vendas/{id}")
+	public void remover(@PathVariable Long id) {
+		vendaRepository.deleteById(id);
 	}
 }
